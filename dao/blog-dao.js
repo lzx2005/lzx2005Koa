89adadfecc9f52dfb2c116f20e4a426da1ffc6d8
李正先 <hzlizx@tairanchina.com>
2018-02-08 22:08:01 +0800

    引入MySQL支持

---
+++ dao/blog-dao.js
@@ -0,0 +1,36 @@
+const mysql = require('mysql');
+const config = require('../config.js');
+const pool  = mysql.createPool({
+    host     : config.database.HOST,
+    user     : config.database.USERNAME,
+    password : config.database.PASSWORD,
+    database : config.database.DATABASE
+});
+
+let query = function( sql, values ) {
+    return new Promise(( resolve, reject ) => {
+        pool.getConnection(function(err, connection) {
+            if (err) {
+                resolve( err )
+            } else {
+                connection.query(sql, values, ( err, rows) => {
+                    if ( err ) {
+                        reject( err )
+                    } else {
+                        resolve( rows )
+                    }
+                    connection.release()
+                })
+            }
+        })
+    })
+};
+
+let findAllBlog = function (  ) {
+    let _sql = `SELECT * FROM blog`;
+    return query( _sql)
+};
+
+module.exports={
+    findAllBlog
+};
--- main.js
+++ main.js
@@ -1,8 +1,26 @@
 const Koa = require('koa');
 const app = new Koa();
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('mysql://root:123456@localhost:3306/lzx2005');
+const Blog = sequelize.define('blog', {
+    blog_id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    title: Sequelize.STRING,
+    author: Sequelize.STRING
+},{
+    tableName: 'blog',
+    underscored: true,
+    createdAt: false,
+    updatedAt: false,
+});
 
 app.use(async ctx => {
-    ctx.body = 'Hello World';
+    let all = Blog.findAll();
+    console.log(all);
+    ctx.body = all;
 });
 
 let port = 3000;
