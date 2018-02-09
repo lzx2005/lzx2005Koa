d42fc1df96e5ce00cd06c3101e2ed4943016ca9a
李正先 <hzlizx@tairanchina.com>
2018-02-09 11:15:21 +0800

    添加koa-router支持，添加README.md

---
+++ README.md
@@ -0,0 +1,17 @@
+# Koa框架搭建的后台
+
+导包
+
+```shell
+npm install
+```
+
+运行
+
+```shell
+node main.js
+```
+
+需求
+
+nodejs > 7
--- main.js
+++ main.js
@@ -1,10 +1,24 @@
 const Koa = require('koa');
 const app = new Koa();
 const blogDao = require("./dao/blog-dao");
+const Router = require('koa-router');
+const router = new Router();
+
+app.use(router.routes());
 
 app.use(async ctx => {
-    let all = await blogDao.Blog.findAll();
-    ctx.body = all;
+    // let all = await blogDao.Blog.findAll();
+    // ctx.body = all;
+    console.log("ssss");
+});
+
+router.get('/hello/:name', async (ctx, next) => {
+    var name = ctx.params.name;
+    ctx.response.body = `<h1>Hello, ${name}!</h1>`;
+});
+
+router.get('/', async (ctx, next) => {
+    ctx.response.body = '<h1>Index</h1>';
 });
 
 let port = 3000;
